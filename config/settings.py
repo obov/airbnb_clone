8426c2ed5a002686e1ede2e927348224a9ffcd6e
obov <obov3e@gmail.com>
2023-01-22 15:35:16 +0900

    django admin panel #4.4

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+CUSTOM_APPS = ["houses.apps.HousesConfig",]
+    
+SYSTEM_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
---
+++ houses/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class House(models.Model):
+
+    """Model Definition for Houses"""
+
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField(verbose_name="price")
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
+    
+    def __str__(self) -> str:
+        return self.name
