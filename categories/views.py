7f77abc32619ae22b624362df70b001956231a32
obov <obov3e@gmail.com>
2023-01-30 14:33:04 +0900

    how to make api / apply rest framework #10.2

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category
 
-# Create your views here.
+
+@api_view()
+def categories(request):
+    return Response(
+        {
+            "ok": True,
+            "categories": Category.objects.all(),
+        }
+    )
