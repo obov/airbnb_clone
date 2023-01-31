d37be98cb5ee1ca79d985901c93b856e57218ea5
obov <obov3e@gmail.com>
2023-01-31 22:54:25 +0900

    serializer #10.6

---
+++ categories/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.CharField(
+        max_length=15,
+    )
+    created_at = serializers.DateTimeField(read_only=True)
--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,33 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
+
+
+@api_view(["GET", "POST"])
+def categories(request):
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"created": True})
+        else:
+            return Response(serializer.errors)
 
 
 @api_view()
-def categories(request):
-    return Response(
-        {
-            "ok": True,
-            "categories": Category.objects.all(),
-        }
-    )
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
+""" 
+    {
+        "name": "Tiny",
+        "kind": "roomsd65as4d65a4sd5a4s"
+    }
+
+
+"""
